3d5cf12ebb7b28d73a9c6f2e33d404120dfc2bc3
chosungwoo <ejyoo12345@naver.com>
2024-01-02 21:47:33 +0900

    Make a GameManager

---
+++ Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/Player/Player.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public float maxHealth;
+    public Transform swordParent;
+
+    private float curHealth;
+
+    private bool isWalk;
+
+    private Animator anim;
+    private Rigidbody2D rigid;
+    private SpriteRenderer sr;
+
+    private Vector2 moveDirection;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        curHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        MoveUpdate();
+        SwordRotationUpdate();
+        AnimationUpdate();
+    }
+
+    private void MoveUpdate()
+    {
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.y = Input.GetAxisRaw("Vertical");
+
+        if (moveDirection.x < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (moveDirection.x > 0)
+        {
+            sr.flipX = false;
+        }
+
+        if (moveDirection != Vector2.zero)
+        {
+            isWalk = true;
+        }
+        else
+        {
+            isWalk = false;
+        }
+
+        rigid.velocity = moveDirection.normalized * moveSpeed;
+    }
+
+    private void SwordRotationUpdate()
+    {
+        var target = transform.position;
+        var mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float angle = Mathf.Atan2(mouse.y - target.y, mouse.x - target.x) * Mathf.Rad2Deg;
+        if (Mathf.Abs(angle) > 90)
+        {
+            swordParent.localScale = new Vector3(1, -1, 1);
+        }
+        else
+        {
+            swordParent.localScale = new Vector3(1, 1, 1);
+        }
+        swordParent.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+
+    private void AnimationUpdate()
+    {
+        anim.SetBool("isWalk", isWalk);
+    }
+
+    public void OnDamge(float damage)
+    {
+        curHealth -= damage;
+
+        if (curHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
